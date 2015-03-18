22e35dff65e63ef8b4a00aad96a23ed8a084fcfe
docodon <docodon@users.noreply.github.com>
2015-03-18 14:29:24 +0530

    Create error.php

---
+++ error.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Either your account doesnot exist  OR  You Have Entered Wrong Password.
+You have entered <?php session_start();
+echo $_SESSION['temp'] ; ?>
+<br>
+ </body>
+ <a href='page1.html'>BACK</a>
+ </html>	
